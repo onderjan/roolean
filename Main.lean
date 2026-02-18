e86f86b79e3baa98620454dd0b56d1c66b05a86a
Jan Onderka <onderjan@onderjan.net>
2026-02-18 19:42:08 +0100

    Add lexing numerals

--- Main.lean
+++ Main.lean
@@ -11,6 +11,7 @@ inductive Character
   | ParenOpen
   -- printable_char
   | ParenClose
+  | Digit (digit: Fin 10)
   -- TODO
   | Other (c: Char)
 deriving Repr
@@ -18,16 +19,19 @@ deriving Repr
 inductive Token
   | ParenOpen
   | ParenClose
+  | Numeral (num: Nat)
   | Text (text: String)
+  -- TODO fail instead of giving an invalid token
+  | Invalid
 deriving Repr
 
 -- Determines the state state of the lexer.
 inductive State
-  -- Stately holding a start of a token.
+  -- State holding a start of a token.
   | Start (token: Token)
-  -- Stately within a comment, waiting for a line break.
+  -- State within a comment, waiting for a line break.
   | Comment
-  -- Stately holds no token and not within a comment.
+  -- State holds no token and not within a comment.
   | Empty
 deriving Repr
 
@@ -40,6 +44,8 @@ def new_state (character: Character): State :=
   -- each parenthesis is a token
   | Character.ParenOpen => State.Start Token.ParenOpen
   | Character.ParenClose => State.Start Token.ParenClose
+  -- digit, start a numeral
+  | Character.Digit d => State.Start (Token.Numeral d)
   -- TODO
   | Character.Other c => State.Start (Token.Text (String.singleton c))
 
@@ -49,6 +55,15 @@ def lex_start_update (token: Token) (next: Character) : (Option Token) × State 
   match token, next with
     -- incoming whitespace, produce the token and clear the state state
     | token, Character.TabSpace | token, Character.LineBreak => (token, State.Empty)
+    -- we have a number, digit incoming
+    | Token.Numeral number, Character.Digit digit =>
+      if number == 0 then
+        -- numbers starting with the digit zero, except for zero, are disallowed
+        -- TODO error
+        (some Token.Invalid, State.Empty)
+      else
+        -- multiply the previous numeral by base and add digit
+        (none, State.Start (Token.Numeral (number * 10 + digit)))
     -- TODO
     | Token.Text text, Character.Other next_char =>
         (none, State.Start (Token.Text (String.push text next_char)))
@@ -78,7 +93,11 @@ structure Lexer where
 def lex_init: Lexer := { state := State.Empty, tokens := List.nil }
 
 def lex_classify (char: Char) : Character :=
-  match char with
+  let code := Char.toNat char
+  let zero_code := Char.toNat '0'
+  if code >= zero_code && code < zero_code + 10 then
+    Character.Digit ((Fin.ofNat 10) (code - zero_code))
+  else match char with
     | '\t' | ' '  => Character.TabSpace
     | '\r' | '\n'  => Character.LineBreak
     | ';' => Character.Semicolon
@@ -109,7 +128,12 @@ def lex_finish (lexer: Lexer) : List Token :=
   -- reverse the list as we added each new token to its start
   List.reverse tokens
 
-def lex: IO (List Token) := do
+def lex_iter {α : Type} [Std.Iterator α Id Char] [Std.IteratorLoop α Id Id]
+  (it: Std.Iter (α:=α) Char): List Token :=
+    let lexer := Std.Iter.fold lex_char lex_init it
+    lex_finish lexer
+
+def process: IO (List Token) := do
   let string ← IO.FS.readFile "benchmarks/addsub.smt2"
   IO.println s!"Read:\n---\n{string}\n---\n"
   let iter := String.chars string
@@ -119,6 +143,8 @@ def lex: IO (List Token) := do
 
 
 def main : IO Unit := do
-  let _discard ← lex
+  let _discard ← process
 
-#eval lex
+#eval lex_iter (List.iter ['5', '7'])
+
+#eval process
