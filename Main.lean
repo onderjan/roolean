3f8816d89cad5ae437ccdc82d41bcf2b6e98b332
Jan Onderka <onderjan@onderjan.net>
2026-02-18 15:31:40 +0100

    Prepare for adding support for comments

--- Main.lean
+++ Main.lean
@@ -1,7 +1,8 @@
 import RooleCheck
 
 inductive Character
-  | Whitespace
+  | TabSpace
+  | LineBreak
   | ParenOpen
   | ParenClose
   | Other (c: Char)
@@ -13,33 +14,52 @@ inductive Token
   | Text (text: String)
 deriving Repr
 
-def new_token (character: Character): Option Token :=
+inductive Current
+  | Start (token: Token)
+  | Empty
+deriving Repr
+
+def new_current (character: Character): Current :=
   match character with
-  | Character.Whitespace => none
-  | Character.ParenOpen => some Token.ParenOpen
-  | Character.ParenClose => some Token.ParenClose
-  | Character.Other c => some (Token.Text (String.singleton c))
+  | Character.TabSpace => Current.Empty
+  | Character.LineBreak => Current.Empty
+  | Character.ParenOpen => Current.Start Token.ParenOpen
+  | Character.ParenClose => Current.Start Token.ParenClose
+  | Character.Other c => Current.Start (Token.Text (String.singleton c))
+
 
 structure Lexer where
-  current: Option Token
+  current: Current
   tokens: (List Token)
 
-def lex_init: Lexer := { tokens := List.nil, current := none }
+def lex_init: Lexer := { current := Current.Empty, tokens := List.nil }
 
 def lex_classify (char: Char) : Character :=
   match char with
     | '(' => Character.ParenOpen
     | ')' => Character.ParenClose
-    | ' ' | '\n'  => Character.Whitespace
+    | '\t' | ' '  => Character.TabSpace
+    | '\r' | '\n'  => Character.LineBreak
     | _ => Character.Other char
 
+def lex_start_update (token: Token) (next: Character) : (Option Token) × Current :=
+  match token, next with
+    | token, Character.TabSpace
+      | token, Character.LineBreak => (token, Current.Empty)
+    | Token.Text text, Character.Other next_char =>
+        (none, Current.Start (Token.Text (String.push text next_char)))
+    | Token.Text text, next  => ((Token.Text text), new_current next)
+    | token,next => (token, new_current next)
 
-def lex_update (current: Option Token) (next: Character) : (Option Token) × (Option Token) :=
-  match current,next with
-    | some (Token.Text text), Character.Other next_char => (none, Token.Text (String.push text next_char))
-    | current, Character.Whitespace => (current, none)
-    | some (Token.Text text), _  => (some (Token.Text text), new_token next)
-    | current,next => (current, new_token next)
+def lex_empty_update (next: Character) : (Option Token) × Current :=
+  match next with
+    | next => (none, new_current next)
+
+
+def lex_update (current: Current) (next: Character) : (Option Token) × Current :=
+  match current with
+    | Current.Start (token) => lex_start_update token next
+    | Current.Empty => (none, new_current next)
 
 def lex_char (lexer: Lexer) (char: Char) : Lexer :=
   let classified := (lex_classify char)
@@ -48,6 +68,7 @@ def lex_char (lexer: Lexer) (char: Char) : Lexer :=
     List.cons previous lexer.tokens
   else
     lexer.tokens
+
   { tokens, current }
 
 def lex_finish (lexer: Lexer) : List Token :=
