b0f54f34d7d4a2da6ae86ca79be5b2d8f4e0d34e
Jan Onderka <onderjan@onderjan.net>
2026-02-25 20:33:15 +0100

    Add assignments generation in checker

--- RooleCheck/QfBv/Checker.lean
+++ RooleCheck/QfBv/Checker.lean
@@ -4,11 +4,54 @@ public import RooleCheck.QfBv.Formula
 
 public structure BitvectorType where
   width: UInt32
-deriving Repr
+deriving Repr, Inhabited
 
 public structure EChecker
 deriving Repr
 
+public structure VarAssignment where
+  type: BitvectorType
+  value: Nat
+deriving Repr, Inhabited
+
+public def checkAssignment (variables: Array BitvectorType) (formula: Formula) (assignment: Nat) : IO (Except EChecker Unit) := do
+  IO.println s!"Assignment: {assignment}"
+
+  let mut assignments: Array VarAssignment := #[]
+  let mut workingAssignment := assignment
+
+  let _ ← for var in variables do
+    let mask := (2 ^ var.width.toNat) - 1
+    let value := workingAssignment &&& mask
+    workingAssignment := workingAssignment >>> var.width.toNat
+    assignments := assignments.push { type := var, value }
+
+  IO.println s!"Assignments: {reprStr assignments}"
+
+  -- let _ ← evaluate formula assignments
+
+  pure (Except.ok ())
+
+
 public def check (variables: Array BitvectorType) (formula: Formula) : IO (Except EChecker Unit) := do
   IO.println s!"Check satisfiability\nVariables: {reprStr variables}\nFormula: {reprStr formula}"
+
+  let totalWidth: Nat := variables.foldl (λ acc e => acc + e.width.toNat) 0
+  let numValues: Nat := 2 ^ totalWidth
+
+  IO.println s!"Num values: {numValues}"
+
+  for assignment in (0...numValues) do
+    let checked ← checkAssignment variables formula assignment
+    match checked with
+      | Except.ok () => pure ()
+      | Except.error {} => return Except.error {}
+
+
+  let mut assignment := Array.replicate variables.size 0
+  IO.println s!"Assignment: {assignment}"
+
+
   pure (Except.ok ())
+
+#eval (3...7).toList
