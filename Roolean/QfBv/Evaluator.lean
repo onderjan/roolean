8a06dd58e6180575c44db32f22ec5ff608d913d0
Jan Onderka <onderjan@onderjan.net>
2026-03-04 19:47:19 +0100

    Rename evaluation functions to shorter names

--- Roolean/QfBv/Evaluator.lean
+++ Roolean/QfBv/Evaluator.lean
@@ -16,24 +16,24 @@ public structure EvalValue (α : Nat → Type) [Domain α] where
 
 mutual
 
-def evaluateUnary {α : Nat → Type} [Domain α]
+def evalUnary {α : Nat → Type} [Domain α]
   (inner: EvalValue α) (op: UniOperator) : EvalValue α :=
   let value := match op with
     | UniOperator.Neg => Domain.uniOp inner.value DomainUniOp.Neg
     | UniOperator.Not => Domain.uniOp inner.value DomainUniOp.Not
   { width := inner.width, value}
 
-def evaluateBiNormal {w} {α : Nat → Type} [Domain α]
+def evalBiNormal {w} {α : Nat → Type} [Domain α]
   (left: α w) (right: α w) (op: DomainBiNormalOp) : EvalValue α :=
   let value := Domain.biNormal left right op
   { width := w, value }
 
-def evaluateBiReduction {w} {α : Nat → Type} [Domain α]
+def evalBiReduction {w} {α : Nat → Type} [Domain α]
   (left: α w) (right: α w) (op: DomainBiReductionOp) : EvalValue α :=
   let value := Domain.biReduction left right op
   { width := 1, value }
 
-def evaluateBinary {α : Nat → Type} [Domain α]
+def evalBinary {α : Nat → Type} [Domain α]
   (left: EvalValue α) (right: EvalValue α) (op: BiOperator) : Except EEvaluator (EvalValue α) := do
 
   let width := left.width
@@ -43,19 +43,19 @@ def evaluateBinary {α : Nat → Type} [Domain α]
     let right: α width := Domain.cast h right.value
 
     let result := match op with
-    | .Add => evaluateBiNormal left right DomainBiNormalOp.Add
-    | .Sub => evaluateBiNormal left right DomainBiNormalOp.Sub
-    | .Mul => evaluateBiNormal left right DomainBiNormalOp.Mul
-    | .Udiv => evaluateBiNormal left right DomainBiNormalOp.Udiv
-    | .Urem => evaluateBiNormal left right DomainBiNormalOp.Urem
-    | .Sdiv => evaluateBiNormal left right DomainBiNormalOp.Sdiv
-    | .Srem => evaluateBiNormal left right DomainBiNormalOp.Srem
+    | .Add => evalBiNormal left right DomainBiNormalOp.Add
+    | .Sub => evalBiNormal left right DomainBiNormalOp.Sub
+    | .Mul => evalBiNormal left right DomainBiNormalOp.Mul
+    | .Udiv => evalBiNormal left right DomainBiNormalOp.Udiv
+    | .Urem => evalBiNormal left right DomainBiNormalOp.Urem
+    | .Sdiv => evalBiNormal left right DomainBiNormalOp.Sdiv
+    | .Srem => evalBiNormal left right DomainBiNormalOp.Srem
 
-    | .BitAnd => evaluateBiNormal left right DomainBiNormalOp.BitAnd
-    | .BitOr => evaluateBiNormal left right DomainBiNormalOp.BitOr
-    | .BitXor => evaluateBiNormal left right DomainBiNormalOp.BitXor
+    | .BitAnd => evalBiNormal left right DomainBiNormalOp.BitAnd
+    | .BitOr => evalBiNormal left right DomainBiNormalOp.BitOr
+    | .BitXor => evalBiNormal left right DomainBiNormalOp.BitXor
 
-    | .Eq => evaluateBiReduction left right DomainBiReductionOp.Eq
+    | .Eq => evalBiReduction left right DomainBiReductionOp.Eq
     | .Ne =>
       let eq := Domain.biReduction left right DomainBiReductionOp.Eq
       let ne := Domain.uniOp eq DomainUniOp.Not
@@ -67,30 +67,30 @@ def evaluateBinary {α : Nat → Type} [Domain α]
       -- we can rewrite to (a or b) == b, which is true exactly
       -- when there is no bit that is set in a but not set in b
       let leftOrRight := Domain.biNormal left right DomainBiNormalOp.BitOr
-      evaluateBiReduction leftOrRight right DomainBiReductionOp.Eq
+      evalBiReduction leftOrRight right DomainBiReductionOp.Eq
 
-    | .Ult => evaluateBiReduction left right DomainBiReductionOp.Ult
-    | .Ule => evaluateBiReduction left right DomainBiReductionOp.Ule
-    | .Slt => evaluateBiReduction left right DomainBiReductionOp.Slt
-    | .Sle => evaluateBiReduction left right DomainBiReductionOp.Sle
+    | .Ult => evalBiReduction left right DomainBiReductionOp.Ult
+    | .Ule => evalBiReduction left right DomainBiReductionOp.Ule
+    | .Slt => evalBiReduction left right DomainBiReductionOp.Slt
+    | .Sle => evalBiReduction left right DomainBiReductionOp.Sle
 
     -- for greater-than and greater-or-equal, flip operands
     -- and use lesser-than / lesser-or-equal
-    | .Ugt => evaluateBiReduction right left DomainBiReductionOp.Ult
-    | .Uge => evaluateBiReduction right left DomainBiReductionOp.Ule
-    | .Sgt => evaluateBiReduction right left DomainBiReductionOp.Slt
-    | .Sge => evaluateBiReduction right left DomainBiReductionOp.Sle
+    | .Ugt => evalBiReduction right left DomainBiReductionOp.Ult
+    | .Uge => evalBiReduction right left DomainBiReductionOp.Ule
+    | .Sgt => evalBiReduction right left DomainBiReductionOp.Slt
+    | .Sge => evalBiReduction right left DomainBiReductionOp.Sle
 
-    | .Shl => evaluateBiNormal left right DomainBiNormalOp.Shl
-    | .Lshr => evaluateBiNormal left right DomainBiNormalOp.Lshr
-    | .Ashr => evaluateBiNormal left right DomainBiNormalOp.Ashr
+    | .Shl => evalBiNormal left right DomainBiNormalOp.Shl
+    | .Lshr => evalBiNormal left right DomainBiNormalOp.Lshr
+    | .Ashr => evalBiNormal left right DomainBiNormalOp.Ashr
 
     Except.ok result
   else
     Except.error EEvaluator.BinaryWidthMismatch
 
 
-def evaluate {α : Nat → Type} [Domain α]
+def eval {α : Nat → Type} [Domain α]
   (formula: Formula) (assignment: Array (EvalValue α)) : Except EEvaluator (EvalValue α) := do
 
   match formula with
@@ -99,13 +99,13 @@ def evaluate {α : Nat → Type} [Domain α]
       Except.ok { width, value }
 
     | Formula.Operation (Operation.Unary op inner) =>
-      let inner ← evaluate inner assignment
-      Except.ok (evaluateUnary inner op)
+      let inner ← eval inner assignment
+      Except.ok (evalUnary inner op)
 
     | Formula.Operation (Operation.Binary op left right) =>
-      let left ← evaluate left assignment
-      let right ← evaluate right assignment
-      evaluateBinary left right op
+      let left ← eval left assignment
+      let right ← eval right assignment
+      evalBinary left right op
 
     | Formula.Variable varIndex =>
       if let some assignment := assignment[varIndex]? then
@@ -117,7 +117,7 @@ end
 
 public def eval3 {α : Nat → Type} [Domain α]
   (formula: Formula) (assignment: Array (EvalValue α)) : Except (EEvaluator) (Option Bool) :=
-  match evaluate formula assignment with
+  match eval formula assignment with
     | Except.ok result =>
       if result.width == 1 then
         match Domain.toBitvector? (result.value) with
