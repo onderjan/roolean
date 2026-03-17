905da821fa4c1d51f48a6c9cd5ad1d27772b3cb4
Jan Onderka <onderjan@onderjan.net>
2026-03-17 14:39:27 +0100

    Simplify modular extreme proof

--- Roolean/QfBv/Domain/ThreeValued/BiNormalOps.lean
+++ Roolean/QfBv/Domain/ThreeValued/BiNormalOps.lean
@@ -8,30 +8,23 @@ namespace Bitvector3
 
  --- DEFINITIONS ---
 
- def extremeMin {w} (left: Bitvector3 w) (right: Bitvector3 w) : Nat :=
-  let minLeft := left.umin.value.toNat
-  let minRight := right.umin.value.toNat
-  minLeft + minRight
+def extremeMin {w} (left: Bitvector3 w) (right: Bitvector3 w) : Nat :=
+  left.umin.value.toNat + right.umin.value.toNat
 
- def extremeMax {w} (left: Bitvector3 w) (right: Bitvector3 w) : Nat :=
-  let minLeft := left.umax.value.toNat
-  let minRight := right.umax.value.toNat
-  minLeft + minRight
+def extremeMax {w} (left: Bitvector3 w) (right: Bitvector3 w) : Nat :=
+  left.umax.value.toNat + right.umax.value.toNat
 
- def extreme {w} (left: Bitvector3 w) (right: Bitvector3 w) (k: Nat) : Option Bool :=
+def extreme {w} (left: Bitvector3 w) (right: Bitvector3 w) (k: Nat) : Option Bool :=
   let min := extremeMin left right
   let max := extremeMax left right
 
-  if min >>> k+1 != max >>> k+1 then
-    none
-  else
+  if min >>> k+1 == max >>> k+1 then
     some (min.testBit k)
+  else
+    none
 
- def modularExtreme {w} (left: Bitvector3 w) (right: Bitvector3 w) (k: Fin w) : Option Bool :=
-  let truncLeft := left.truncate k.isLt
-  let truncRight := right.truncate k.isLt
-
-  extreme truncLeft truncRight k
+def modularExtreme {w} (left: Bitvector3 w) (right: Bitvector3 w) (k: Fin w) : Option Bool :=
+  extreme (left.truncate k.isLt) (right.truncate k.isLt) k
 
 public def biNormal {w} (left: Bitvector3 w) (right: Bitvector3 w) (op: BiNormalOp)
   : Bitvector3 w :=
@@ -65,47 +58,41 @@ public def biNormal {w} (left: Bitvector3 w) (right: Bitvector3 w) (op: BiNormal
 theorem extremeMin_sound {w} (a b: Bitvector3 w) (ca cb: Bitvector w)
   : γ a ca → γ b cb → extremeMin a b ≤ ca.value.toNat + cb.value.toNat := by
   intro ha hb
-  simp[extremeMin]
-  let hUminA := umin_le_γ a ca ha
-  let hUminB := umin_le_γ b cb hb
-  rw[BitVec.le_def] at hUminA hUminB
-  simp[hUminA, hUminB, Nat.add_le_add]
+  let hUminA := Iff.mp BitVec.le_def (umin_le_γ a ca ha)
+  let hUminB := Iff.mp BitVec.le_def (umin_le_γ b cb hb)
+  simp[extremeMin, hUminA, hUminB, Nat.add_le_add]
 
 theorem extremeMax_sound {w} (a b: Bitvector3 w) (ca cb: Bitvector w)
   : γ a ca → γ b cb → ca.value.toNat + cb.value.toNat ≤ extremeMax a b := by
   intro ha hb
-  simp[extremeMax]
-  let hUmaxA := γ_le_umax a ca ha
-  let hUmaxB := γ_le_umax b cb hb
-  rw[BitVec.le_def] at hUmaxA hUmaxB
-  simp[hUmaxA, hUmaxB, Nat.add_le_add]
+  let hUmaxA := Iff.mp BitVec.le_def (γ_le_umax a ca ha)
+  let hUmaxB := Iff.mp BitVec.le_def (γ_le_umax b cb hb)
+  simp[extremeMax, hUmaxA, hUmaxB, Nat.add_le_add]
 
 theorem extreme_soundNat {w} (a b: Bitvector3 w) (ca cb: Bitvector w) (k: Nat) (r: Bool)
   : γ a ca → γ b cb → extreme a b k = some r → r = (ca.value.toNat + cb.value.toNat).testBit k := by
   intro ha hb hExtreme
   simp[extreme] at hExtreme
-  let hMin := extremeMin_sound a b ca cb ha hb
-  let hMax := extremeMax_sound a b ca cb ha hb
-  let h1 := hExtreme.left
-  let h2 := hExtreme.right
-  simp[← h2]
+  simp[← hExtreme.right]
   let hLe : 2^k ≤ 2^k := by simp
   let hNeZero : 2^k ≠ 0 := by simp
+
+  let hMin := extremeMin_sound a b ca cb ha hb
   let hMin := Nat.div_le_div hMin hLe hNeZero
   simp[← Nat.shiftRight_eq_div_pow] at hMin
+
+  let hMax := extremeMax_sound a b ca cb ha hb
   let hMax := Nat.div_le_div hMax hLe hNeZero
   simp[← Nat.shiftRight_eq_div_pow] at hMax
+
   simp[← hExtreme] at hMax
-  let hConcrete := Nat.le_antisymm hMin hMax
-  simp[Nat.testBit]
-  simp[← hConcrete]
+  simp[Nat.testBit, ← Nat.le_antisymm hMin hMax]
 
 theorem extreme_sound {w} (a b: Bitvector3 w) (ca cb: Bitvector w) (k: Fin w) (r: Bool)
   : γ a ca → γ b cb → extreme a b k = some r → r = (ca.value + cb.value)[k] := by
   intro ha hb hExtreme
   simp[BitVec.add_def, BitVec.ofNat]
   exact extreme_soundNat a b ca cb k r ha hb hExtreme
-
 
 theorem modularExtreme_sound {w} (a b: Bitvector3 w) (ca cb: Bitvector w) (k: Fin w) (r: Bool)
   : γ a ca → γ b cb → modularExtreme a b k = some r → r = (ca.value + cb.value)[k] := by
