d77e37574e523c94a265e2a61d2fde1eab0c8c22
Jan Onderka <onderjan@onderjan.net>
2026-03-24 15:40:37 +0100

    Start generalising the Shl proof

--- Roolean/QfBv/Domain/Bitvector3/BiNormalOp/Shift.lean
+++ Roolean/QfBv/Domain/Bitvector3/BiNormalOp/Shift.lean
@@ -27,23 +27,27 @@ def shlByConst {w} (a: Bitvector3 w) (b: Nat) : Bitvector3 w :=
 
   { zeros, ones, zeros_or_ones_set }
 
-def shlRec {w} (a b: Bitvector3 w) (z: Bitvector3 w) (bMin: Nat) (bAdd: Nat): Bitvector3 w :=
+def shiftRec {w} (f: Bitvector3 w → Nat → Bitvector3 w)
+  (a b: Bitvector3 w) (z: Bitvector3 w) (bMin: Nat) (bAdd: Nat): Bitvector3 w :=
   let next := if γ b { value := bMin + bAdd } then
-    z.join (a.shlByConst (bMin + bAdd))
+    z.join (f a (bMin + bAdd))
   else
     z
   if bAdd = 0 then
     next
   else
-    shlRec a b next bMin (bAdd-1)
+    shiftRec f a b next bMin (bAdd-1)
 
-public def shl {w} (a b: Bitvector3 w): Bitvector3 w :=
+public def shift {w} (f: Bitvector3 w → Nat → Bitvector3 w)
+  (a b: Bitvector3 w): Bitvector3 w :=
   let min := b.umin.value.toNat
   let max := b.umax.value.toNat
   -- for simplicity, we will construct the initial bitvector here
   -- so we do not have to handle it in recursive calls
   -- we will then call recursively
-  shlRec a b (shlByConst a max) min (max-min)
+  shiftRec f a b (f a max) min (max-min)
+
+public def shl {w} (a b: Bitvector3 w): Bitvector3 w := shift shlByConst a b
 
 --- THEOREMS ---
 
@@ -77,23 +81,24 @@ theorem shlByConst_sound  {w} (a: Bitvector3 w) (shift: Nat) (c: Bitvector w)
   }
 
 
-def shlRec_start_monotone {w} (a b: Bitvector3 w) (bMin bAdd: Nat)
-  : ∀x, ∀c, γ x c → γ (shlRec a b x bMin bAdd) c := by
+def shiftRec_start_monotone {w} (f: Bitvector3 w → Nat → Bitvector3 w)
+  (a b: Bitvector3 w) (bMin bAdd: Nat)
+  : ∀x, ∀c, γ x c → γ (shiftRec f a b x bMin bAdd) c := by
   induction bAdd with
   | zero => {
     intro x c h
-    rw[shlRec]
+    rw[shiftRec]
     simp
     split
-    { exact γ_join_left x (a.shlByConst bMin) c h }
+    { exact γ_join_left x (f a bMin) c h }
     { exact h }
   }
   | succ n hIn => {
     intro x c h
-    rw[shlRec]; simp
+    rw[shiftRec]; simp
     split
     {
-      let joinVal := (a.shlByConst (bMin + (n + 1)))
+      let joinVal := (f a (bMin + (n + 1)))
       let hRec := hIn (x.join joinVal) c (γ_join_left x joinVal c h)
       simp[joinVal] at hRec
       exact hRec
@@ -101,22 +106,23 @@ def shlRec_start_monotone {w} (a b: Bitvector3 w) (bMin bAdd: Nat)
     { exact hIn x c h }
   }
 
-def shlRec_sound {w} (a b: Bitvector3 w) (bMin: Nat) (bAdd: Nat)
+def shiftRec_sound {w} (f: Bitvector3 w → Nat → Bitvector3 w)
+  (a b: Bitvector3 w) (bMin: Nat) (bAdd: Nat)
   : ∀s, s ≤ bAdd → γ b { value := BitVec.ofNat w (bMin) + BitVec.ofNat w s } →
-    ∀c, γ (shlByConst a (bMin + s)) c → ∀z, γ (shlRec a b z bMin bAdd) c := by
+    ∀c, γ (f a (bMin + s)) c → ∀z, γ (shiftRec f a b z bMin bAdd) c := by
   induction bAdd with
   | zero => {
 
     simp
     intro hBS c hShl z
-    simp[shlRec]
+    simp[shiftRec]
     split
     { simp[γ_join_right, hShl] }
     { rename_i hContra; simp[hContra] at hBS }
   }
   | succ n hIn => {
     intro s hS hBS c hShl z
-    rw[shlRec]; simp
+    rw[shiftRec]; simp
 
     split
     {
@@ -125,13 +131,13 @@ def shlRec_sound {w} (a b: Bitvector3 w) (bMin: Nat) (bAdd: Nat)
       by_cases s ≤ n
       {
         rename_i hST
-        exact hIn s hST hBS c hShl (z.join (a.shlByConst (bMin + (n + 1))))
+        exact hIn s hST hBS c hShl (z.join (f a (bMin + (n + 1))))
       }
       {
         let hS : s = n+1 := by grind
         simp[hS] at hBS; simp[hS] at hShl
-        let hJoined := γ_join_right z (a.shlByConst (bMin + (n+1))) c hShl
-        exact shlRec_start_monotone a b bMin n (z.join (a.shlByConst (bMin + (n + 1)))) c hJoined
+        let hJoined := γ_join_right z (f a (bMin + (n+1))) c hShl
+        exact shiftRec_start_monotone f a b bMin n (z.join (f a (bMin + (n + 1)))) c hJoined
       }
     }
     {
@@ -152,17 +158,19 @@ def shlRec_sound {w} (a b: Bitvector3 w) (bMin: Nat) (bAdd: Nat)
     }
   }
 
-public def shl_sound {w} (a b: Bitvector3 w) (ca cb: Bitvector w)
-    : γ a ca → γ b cb → γ (shl a b) (Bitvector.biNormal ca cb BiNormalOp.Shl) := by
+def shift_sound {w} (f: Bitvector3 w → Nat → Bitvector3 w)
+  (hF: (a: Bitvector3 w) → (s: Nat) → (c: Bitvector w) → γ a c → γ (f a s) { value := (c.value <<< s) })
+  (a b: Bitvector3 w) (ca cb: Bitvector w)
+    : γ a ca → γ b cb → γ (shift f a b) (Bitvector.biNormal ca cb BiNormalOp.Shl) := by
   intro ha hb
   simp[Bitvector.biNormal, Bitvector.standardBi]
-  simp[shl]
+  simp[shift]
   let hUminLe := umin_le_γ b cb hb
   let hLeUmax := γ_le_umax b cb hb
   let hUminLeUmax := umin_le_umax b
-  let hByConst := shlByConst_sound a cb.value.toNat ca ha
+  --let hByConst := shlByConst_sound a cb.value.toNat ca ha
 
-  let hRec := shlRec_sound a b (b.umin.toNat) (b.umax.toNat-b.umin.toNat) (cb.toNat-b.umin.toNat)
+  let hRec := shiftRec_sound f a b (b.umin.toNat) (b.umax.toNat-b.umin.toNat) (cb.toNat-b.umin.toNat)
   simp[BitVec.le_def] at hLeUmax
   simp[Bitvector.toNat] at hRec
   simp[Nat.sub_add_cancel hUminLeUmax, hLeUmax] at hRec
@@ -179,4 +187,12 @@ public def shl_sound {w} (a b: Bitvector3 w) (ca cb: Bitvector w)
 
   simp[hCancelBitVec, hCancelNat] at hRec
 
-  exact hRec hb { value := ca.value <<< cb.toNat } hByConst (a.shlByConst b.umax.toNat)
+  let hByConst := hF a cb.value.toNat { value := ca.value } ha
+
+  exact hRec hb { value := ca.value <<< cb.toNat } hByConst (f a b.umax.toNat)
+
+
+public def shl_sound {w} (a b: Bitvector3 w) (ca cb: Bitvector w)
+    : γ a ca → γ b cb → γ (shl a b) (Bitvector.biNormal ca cb BiNormalOp.Shl) := by
+  intro ha hb; simp[shl]
+  exact shift_sound shlByConst shlByConst_sound a b ca cb ha hb
